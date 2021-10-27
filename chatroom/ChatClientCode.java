79da082f614f5de0795f92e3f74a9c545576fa2c
mahases <mahases@BY2024A98603810.wit.private>
2021-10-27 11:29:42 -0400

    added Chat room functionality

--- chatroom/ChatClientCode.java
+++ chatroom/ChatClientCode.java
@@ -1,10 +1,54 @@
-package lab4.chatroom;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class ChatClientCode {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static class Chatroom implements Runnable {
+		DataOutputStream outToServer;
+		BufferedReader inFromServer;
+		
+		Chatroom(DataOutputStream outToServer,BufferedReader inFromServer){
+			this.outToServer = outToServer;
+			this.inFromServer = inFromServer;
+		}
+		
+		@Override
+		public void run() {
+		while(true) {
+			try {
+				String modifiedMessage;
+				modifiedMessage = inFromServer.readLine();
+				System.out.println(modifiedMessage);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				break;
+			}
+		}
 	}
+}
+	public static void main(String argv[]) throws Exception {
+		Socket connectionSocket = new Socket("localhost", 1234);
 
+		DataOutputStream outToServer = new DataOutputStream(connectionSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+		
+		Chatroom chatRoom = new Chatroom(outToServer,inFromServer);
+		Thread thread = new Thread(chatRoom);
+		thread.start();
+		System.out.println("Welcome to the chat room! before you can begin please tell us your name.");
+		Scanner s = new Scanner(System.in);
+		String message = s.nextLine();
+		outToServer.writeBytes(message + "\r\n");
+		System.out.println("You are now in the chat room! If you wish to leave please say \"quit\".");
+		while(!message.toUpperCase().equals("QUIT")) {
+			message = s.nextLine();
+			outToServer.writeBytes(message + "\r\n");
+		}
+		s.close();
+		outToServer.close();
+		connectionSocket.close();
+	}
 }
--- chatroom/ChatServerCode.java
+++ chatroom/ChatServerCode.java
@@ -1,4 +1,4 @@
-package lab4.chatroom;
+
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -6,67 +6,137 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServerCode {
 
+	static List<People> persons = new ArrayList<>();
+	static List<String> prevMsg = new ArrayList<>();
 	static class ClientRequest implements Runnable {
-		
 		Socket connectionSocket;
 		
-		public ClientRequest(Socket s) {
-			this.connectionSocket = s;
+		ClientRequest(Socket c){
+			connectionSocket = c;
+			
 		}
-	
+		
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			try {
-				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-				DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
-				String clientMessage = inFromClient.readLine();
-				System.out.println("RECEIVED: " + clientMessage);
-
-				String modifiedMessage = clientMessage.toUpperCase() + "\r\n";
-				outToClient.writeBytes(modifiedMessage);
-				this.connectionSocket.close();
+			String name;
+			try{
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			
+			String clientMessage ="";
+			name = inFromClient.readLine();
+			for(int i = 0; i < persons.size();i++) {
+				if(persons.get(i).getConnection()==connectionSocket) {
+					persons.get(i).setName(name);
+				}
 			}
-			catch (Exception ex) {
-				
+			for(int i = 0; i < prevMsg.size();i++) {
+				outToClient.writeBytes(prevMsg.get(i));
+			}
+			sendToAll(name+" has joined the chat!\n");
+			
+			while(true) {
+				clientMessage = inFromClient.readLine();
+				System.out.println("RECEIVED: " + clientMessage);
+				if(clientMessage.toUpperCase().equals("QUIT")) {
+					break;
+				}
+				String modifiedMessage = clientMessage + "\r\n";	
+				sendToAll(modifiedMessage,name);
 			}
 			
+			sendToAll(name + " has left the chat!\n");
+			closeConnection(name);
+			inFromClient.close();
+			outToClient.close();
+			}
+			catch(Exception ex) {
+				ex.printStackTrace();
+			}
 			
 		}
 		
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		ServerSocket serverSocket;
-		try {
-			serverSocket = new ServerSocket(1234);
-			System.out.println("This server is ready to receive");
+	public static void main(String argv[]) throws Exception {
+		ServerSocket serverSocket = new ServerSocket(1234);
 
-			while (true) {
-				Socket connectionSocket = serverSocket.accept();
-				ClientRequest clientRequest = new ClientRequest(connectionSocket);
-				Thread thread = new Thread(clientRequest);
-				thread.start();
-				
-				
-				
-			
+		System.out.println("This server is ready to receive");
+
+		while (true) {
+			Socket connectionSocket = serverSocket.accept();
+			ClientRequest clientRequest = new ClientRequest(connectionSocket);
+			Thread thread = new Thread(clientRequest);
+			thread.start();
+			persons.add(new People(connectionSocket));
+		}
+	}
+	
+	public static void sendToAll(String msg, String name) throws IOException {
+		if(prevMsg.size() < 5) {
+			prevMsg.add(name+":"+msg);
+		}
+		else {
+			prevMsg.remove(0);
+			prevMsg.add(name+":"+msg);
+		}
+		for(int i = 0; i < persons.size();i++) {
+			if(name.equals(persons.get(i).getName())) {
+				continue;
+			}
+			DataOutputStream outToClient = new DataOutputStream(persons.get(i).getConnection().getOutputStream());
+			outToClient.writeBytes(name+":"+msg);
+		}
+	}
+	
+	public static void sendToAll(String msg) throws IOException {
+		if(prevMsg.size() < 5) {
+			prevMsg.add(msg);
+		}
+		else {
+			prevMsg.remove(0);
+			prevMsg.add(msg);
+		}
+		for(int i = 0; i < persons.size();i++) {
+			DataOutputStream outToClient = new DataOutputStream(persons.get(i).getConnection().getOutputStream());
+			outToClient.writeBytes(msg);
+		}
+	}
+
+	public static void closeConnection(String name) {
+		for(int i = 0; i < persons.size();i++) {
+			if(name.equals(persons.get(i).getName())) {
+				persons.remove(i);
 			}
 		}
-			catch (Exception ex) {
-			// TODO Auto-generated catch block
-			System.out.println("Error!\r\n");
-		}
-
+	}
 	
+	static class People {
+		String name;
+		Socket connection;
+		
+		People(Socket c){
+			connection = c;
 		}
-
+		
+		public void setName(String n) {
+			name = n;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public Socket getConnection() {
+			return connection;
+		}
+	}
+	
 	}
 
 
